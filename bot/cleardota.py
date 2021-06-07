155e921e8d5994bfa2c3d17d2a46ad0e50f3a57a
Will (Craigsuter2) <>
2021-06-07 15:41:48 +0000

    t2

--- bot/cleardota.py
+++ bot/cleardota.py
@@ -1,6 +1,7 @@
 import os
 #imports
 from bs4 import BeautifulSoup as soup
+from pyvirtualdisplay import Display
 from dotenv import load_dotenv
 load_dotenv()
 import requests
@@ -14,11 +15,11 @@ def cleardota():
   print("hi")
   
   
-
+  display = Display(visible=False, size=(800, 600))
+  display.start()
 
   chrome_options = webdriver.ChromeOptions()
   chrome_options.binary_location=os.environ.get("GOOGLE_CHROME_BIN")
-  chrome_options.add_argument("start-maximized")
   chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36")
   chrome_options.add_argument("--disable-dev-shm-usage")
   chrome_options.add_argument("--no-sandbox")
@@ -67,4 +68,5 @@ def cleardota():
   button.click()
 
   driver.close()
+  display.stop
   
