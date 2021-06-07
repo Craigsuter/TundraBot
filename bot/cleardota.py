4c6aea4cd0d8287bbe35d4844755011de96f720c
Will (Craigsuter2) <>
2021-06-07 15:09:13 +0000

    sel bin

--- bot/cleardota.py
+++ bot/cleardota.py
@@ -14,7 +14,7 @@ def cleardota():
   print("hi")
  
   chrome_options = webdriver.ChromeOptions()
-  chrome_options.binary_location=os.environ.get("GOOGLE_CHROME_BINARY")
+  chrome_options.binary_location=os.environ.get("GOOGLE_CHROME_BIN")
   chrome_options.add_argument("--headless")
   chrome_options.add_argument("--disable-dev-shm-usage")
   chrome_options.add_argument("--no-sandbox")
