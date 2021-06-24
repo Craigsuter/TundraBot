1559551e3062a44f6466f3f1c037f54457551484
Will (Craigsuter2) <willcrane78@gmail.com>
2021-06-24 23:28:55 +0000

    !playerinfo patch

--- bot/main.py
+++ bot/main.py
@@ -684,7 +684,7 @@ async def on_message(message):
           if(generalinfo != ""):
             embed.add_field(name = "General information", value = generalinfo)
             counter1 = counter1 +1
-          if(playerinfo != "" or playerinfotest.strip() != "**__Team History__**"):
+          if(playerinfotest != "**__Team History__**"):
             embed.add_field(name = "Player information", value = playerinfo, inline= False)
             counter2 = counter2 +1
           
