59e271c4123fc553834294b0781b55445d8f43fd
Will (Craigsuter2) <willcrane78@gmail.com>
2021-12-20 13:02:07 +0000

    fixeroo

--- bot/cleardota.py
+++ bot/cleardota.py
@@ -20,7 +20,7 @@ def cleardota():
   chrome_options.add_argument("--headless")
   chrome_options.add_argument("--disable-dev-shm-usage")
   chrome_options.add_argument("--no-sandbox")
-  chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36")
+  chrome_options.add_argument("user-agent=Chrome/76.0.3809.100")
   driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 
