569ce961fb0b5a286cebda39edbdd2deb423c007
Will (Craigsuter2) <willcrane78@gmail.com>
2021-12-20 01:06:40 +0000

    trying firefox

--- bot/cleardota.py
+++ bot/cleardota.py
@@ -5,67 +5,54 @@ from dotenv import load_dotenv
 load_dotenv()
 import requests
 from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import time
 
 def cleardota():
-  #Loading username / password for Liquipedia 1
+  #Loading username / password for Liquipedia
   LiquiUser =  os.getenv('liquiUser')
   liquiPass = os.getenv('liquiPass')
   print("hi")
+  #Opening web browser / logging on
+  #options = Options()
+  #options.add_arugment('--headless')
+  options = webdriver.FirefoxOptions()  
+  options.add_argument('--disable-gpu')
+  options.add_argument('--no-sandbox') 
+  #options.headless = True
+  options.add_argument('--headless')
+ 
+  print("hi2")
+  driver = webdriver.Firefox(options=options)
+  
+  driver.get("https://liquipedia.net/dota2/index.php?title=Special:UserLogin&returnto=OG&returntoquery=action%3Dedit")
+  print("hi2")
+  #Finds the username box and types the username in
+  button = driver.find_element_by_id('wpName1')
+  button.click()
+  button.send_keys(LiquiUser)
+  print("hi3")
 
-  try:
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location=os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    the_path = os.environ.get("CHROMEDRIVER_PATH")
-    driver = webdriver.Chrome(executable_path=the_path, chrome_options=chrome_options)
+  #Finds the password box and types the password in
+  button=driver.find_element_by_id('wpPassword1')
+  button.click()
+  button.send_keys(liquiPass)
 
+  button.submit()
 
-    print("hi2")
-    #driver = webdriver.Firefox(options=options)
-    
-    driver.get("https://liquipedia.net/dota2/index.php?title=Special:UserLogin&returnto=OG&returntoquery=action%3Dedit")
-    print("hi2")
-    #Finds the username box and types the username in
-    try:
-      button = driver.find_element_by_id('wpName1')
-      button.click()
-      button.send_keys(LiquiUser)
-      print("hi3")
-      
-      time.sleep(5)
+  #Going to OG's Liquipedia page
 
-      #Finds the password box and types the password in
-      button=driver.find_element_by_id('wpPassword1')
-      button.click()
-      button.send_keys(liquiPass)
+  time.sleep(5)
+  driver.get('https://liquipedia.net/dota2/OG')
+  time.sleep(10)
+  button=driver.find_element_by_id('ca-purge')
+  button.click()
 
-      button.submit()
-    except:
-      print("Error logging in")
+  time.sleep(17)
+  #Finding the clear cache button and then logging out
+  button=driver.find_element_by_class_name('fas.fa-fw.fa-user-circle')
+  button.click()
 
-    #Going to OG's Liquipedia page
+  button=driver.find_element_by_id('pt-logout')
+  button.click()
 
-    time.sleep(5)
-    driver.get('https://liquipedia.net/dota2/OG')
-    print("hi4")
-    print(driver.page_source)
-    time.sleep(10)
-    button=driver.find_element_by_id('ca-purge')
-    print("hi5")
-    button.click()
-
-    time.sleep(17)
-    #Finding the clear cache button and then logging out
-    print("hi6")
-    button=driver.find_element_by_class_name('fas.fa-fw.fa-user-circle')
-    button.click()
-
-    button=driver.find_element_by_id('pt-logout')
-    button.click()
-
-    driver.close()
-  except Exception as e: print(e)
+  driver.close()
