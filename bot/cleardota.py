b2a8d8edeb6ec8b57562f7bba85e0717de7690e3
Will (Craigsuter2) <willcrane78@gmail.com>
2021-12-20 12:58:14 +0000

    chrome

--- bot/cleardota.py
+++ bot/cleardota.py
@@ -9,36 +9,34 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import time
 
 def cleardota():
-  try:
-    #Loading username / password for Liquipedia
-    LiquiUser =  os.getenv('liquiUser')
-    liquiPass = os.getenv('liquiPass')
-    print("hi")
-    #Opening web browser / logging on
-    #options = Options()
-    #options.add_arugment('--headless')
-    options = webdriver.FirefoxOptions()  
-    options.add_argument('-disable-gpu')
-    options.add_argument('-no-sandbox') 
-    #options.headless = True
-    options.add_argument('-headless')
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference("general.useragent.override","user-agent = Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36")
-    binary = FirefoxBinary(os.environ.get('FIREFOX_BIN'))
-    driver = webdriver.Firefox(firefox_profile= profile, firefox_binary=binary, executable_path=os.environ.get('GECKODRIVER_PATH'), options=options)
-  
-    print("hi2")
-    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
+  #Loading username / password for Liquipedia
+  LiquiUser =  os.getenv('liquiUser')
+  liquiPass = os.getenv('liquiPass')
+  print("hi")
 
+
+  chrome_options = webdriver.ChromeOptions()
+  chrome_options.binary_location=os.environ.get("GOOGLE_CHROME_BIN")
+  chrome_options.add_argument("--headless")
+  chrome_options.add_argument("--disable-dev-shm-usage")
+  chrome_options.add_argument("--no-sandbox")
+  chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36")
+  driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+
+  print("hi2")
+  #driver = webdriver.Firefox(options=options)
   
-    
-    driver.get("https://liquipedia.net/dota2/index.php?title=Special:UserLogin&returnto=OG&returntoquery=action%3Dedit")
-    print("hi2")
-    #Finds the username box and types the username in
+  driver.get("https://liquipedia.net/dota2/index.php?title=Special:UserLogin&returnto=OG&returntoquery=action%3Dedit")
+  print("hi2")
+  #Finds the username box and types the username in
+  try:
     button = driver.find_element_by_id('wpName1')
     button.click()
     button.send_keys(LiquiUser)
     print("hi3")
+    
+    time.sleep(5)
 
     #Finds the password box and types the password in
     button=driver.find_element_by_id('wpPassword1')
@@ -46,18 +44,22 @@ def cleardota():
     button.send_keys(liquiPass)
 
     button.submit()
+    
 
     #Going to OG's Liquipedia page
 
     time.sleep(5)
-    text = driver.get('https://liquipedia.net/dota2/OG')
+    driver.get('https://liquipedia.net/dota2/OG')
+    print("hi4")
+    print(driver.page_source)
     time.sleep(10)
-    print(text)
     button=driver.find_element_by_id('ca-purge')
+    print("hi5")
     button.click()
 
     time.sleep(17)
     #Finding the clear cache button and then logging out
+    print("hi6")
     button=driver.find_element_by_class_name('fas.fa-fw.fa-user-circle')
     button.click()
 
@@ -65,4 +67,5 @@ def cleardota():
     button.click()
 
     driver.close()
-  except Exception as e: print(e)
+  except Exception as e:
+    print(e)
