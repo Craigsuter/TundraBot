baba6554525cac2fa4505ed76a5058a10ec66637
Will (Craigsuter2) <willcrane78@gmail.com>
2022-01-30 02:15:17 +0000

    cleaning up the csmaps page

--- bot/csmap.py
+++ bot/csmap.py
@@ -53,6 +53,8 @@ def csgomap():
     test5 = test2.findAll("div", {"class":"results-teamname text-ellipsis"})
     test4 = test2.findAll("div", {"class":"results-team-score"})
     test3 = test2.findAll("div", {"class":"mapname"})
+
+    #creating variables
     i=0
     j=0
     k=0
@@ -77,13 +79,13 @@ def csgomap():
         else:
           scoresteam2.append(scoredata)
         j=j+1
-      
+      #collecting the teams
       while(z < 2):
         team = test5[z].text
         teamnames.append(team)
         z=z+1
       
-
+      #checking for unplayed maps
       for counter, n in enumerate(scoresteam1):
         if n == "-":
           scoresteam1[counter] = "0"
@@ -91,12 +93,15 @@ def csgomap():
         if n == "-":
           scoresteam2[counter] = "0"
 
+
+      #generating the message
       while(k < len(maps)):
         if k == 0:
           messagetosend =  maps[k] + " (||" + teamnames[0] + " " + scoresteam1[k] + " - " + scoresteam2[k] + " " + teamnames[1] + "||)"
         else:
           messagetosend = messagetosend + ", " + maps[k] + " (||" + teamnames[0] + " " + scoresteam1[k] + " - " + scoresteam2[k] + " " + teamnames[1] + "||)"
         k=k+1
+    #For if the maps are not announced yet
     else:
       i=0
       while i < len(maps):
@@ -106,16 +111,7 @@ def csgomap():
           messagetosend = messagetosend + ", " + maps[i]
         i = i+1
       
-    
 
-
-
-      
-    
-    
-
-
-    
     return(messagetosend)
 
 
