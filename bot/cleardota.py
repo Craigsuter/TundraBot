e9fe572b00c422349228241854336b4d2825d4cb
Will (Craigsuter2) <willcrane78@gmail.com>
2022-02-22 09:31:55 +0000

    more hash

--- bot/cleardota.py
+++ bot/cleardota.py
@@ -159,10 +159,10 @@ def dotaplayerstats(name):
   chrome_options.add_argument("--window-size=1920,1080")
 
   # you need executable path for heroku (aka production) - but remove it for using replit 
-  # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+  driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
   
   # Use this for testing
-  driver = webdriver.Chrome(chrome_options=chrome_options)
+  #driver = webdriver.Chrome(chrome_options=chrome_options)
   name2 = name.lower()
 
   driver.get("https://dota2protracker.com/")
