0064a9894235d10f95fb0bed234a220d979e8de0
Will (Craigsuter2) <willcrane78@gmail.com>
2022-02-22 10:05:20 +0000

    updated to check wl_record

--- bot/cleardota.py
+++ bot/cleardota.py
@@ -183,30 +183,32 @@ def dotaplayerstats(name):
     link = container[0].find("a")['href']
     link = "https://dota2protracker.com" + link
     driver.close()
-    print(link)
     player_page = requests.get(link, headers=headers)
-    print(player_page)
     page_html = soup(player_page.text, "html.parser")
-    print("3")
+
+
     wl_record = page_html.find("div", {"class": "player_stats"}).find("span").text
-    print("2")
-    player_picks = page_html.find_all("div", {"class": "meta-hero-card"})
-    print("1")
-    top5_picks_container = player_picks[:5]
+    if(int(wl_record[0]) > 0):
+      player_picks = page_html.find_all("div", {"class": "meta-hero-card"})
+      top5_picks_container = player_picks[:5]
 
-    top5_picks_info = ""
+      top5_picks_info = ""
 
-    for pick_container in top5_picks_container:
-        pick_info = pick_container.find_all("div", {"class": "meta-pick-info-block"})
-        pick_name = pick_container.find("div", {"class": "meta-pick-title"}).text
-        pick_matchcount = pick_info[0].next_element.strip()
-        pick_winrate = pick_info[1].next_element.strip()
-        top5_picks_info = top5_picks_info + \
-            f'{pick_name}: {pick_matchcount} matches ({pick_winrate} Winrate) \n'
+      for pick_container in top5_picks_container:
+          pick_info = pick_container.find_all("div", {"class": "meta-pick-info-block"})
+          pick_name = pick_container.find("div", {"class": "meta-pick-title"}).text
+          pick_matchcount = pick_info[0].next_element.strip()
+          pick_winrate = pick_info[1].next_element.strip()
+          top5_picks_info = top5_picks_info + \
+              f'{pick_name}: {pick_matchcount} matches ({pick_winrate} Winrate) \n'
 
-    player_stats = discord.Embed(title=f'{name} stats', url=link, color=0x55a7f7)
-    player_stats.add_field(name="Win Lose", value=wl_record, inline=False)
-    player_stats.add_field(name="Top 5 picks", value=top5_picks_info, inline=False)
+      player_stats = discord.Embed(title=f'{name} stats', url=link, color=0x55a7f7)
+      player_stats.add_field(name="Win Lose", value=wl_record, inline=False)
+      player_stats.add_field(name="Top 5 picks", value=top5_picks_info, inline=False)
+    else:
+      player_stats = discord.Embed(title=f'{name} stats', url=link, color=0x55a7f7)
+      player_stats.add_field(name="Win Lose", value=wl_record, inline=False)
+      
     
     return player_stats
   except Exception as e:
