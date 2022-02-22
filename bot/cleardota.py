5ea41f87598aa08dc1d75419eacb046b4f13efa8
Will (Craigsuter2) <willcrane78@gmail.com>
2022-02-22 09:31:36 +0000

    welp

--- bot/cleardota.py
+++ bot/cleardota.py
@@ -153,7 +153,7 @@ def dotaplayerstats(name):
   headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
   chrome_options = webdriver.ChromeOptions()
   chrome_options.binary_location=os.environ.get("GOOGLE_CHROME_BIN")
-  #chrome_options.add_argument("--headless") 
+  chrome_options.add_argument("--headless") 
   chrome_options.add_argument("--disable-dev-shm-usage")
   chrome_options.add_argument("--no-sandbox")
   chrome_options.add_argument("--window-size=1920,1080")
