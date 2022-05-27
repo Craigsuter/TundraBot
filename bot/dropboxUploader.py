9e3a0c2453b4fb3a3d915825213fe4e1b80af66e
Will (Craigsuter2) <willcrane78@gmail.com>
2022-05-27 07:07:45 +0000

    fixed dropbox for tundra bot + removed all commands not wanted

--- bot/dropboxUploader.py
+++ bot/dropboxUploader.py
@@ -4,12 +4,17 @@ import os
 dbx = dropbox.Dropbox(os.getenv('DROPBOX_ACCESS_TOKEN'))
 
 def download_file(filename, savehere):
+  filename= filename[1:]
+  filename = "/tundra" + filename
+  
   dbx.files_download_to_file(savehere, filename)
 
   
 
 
 def upload_file(filename, localfile):
-
+  filename = filename[1:]
+  filename = "/tundra" + filename
+  
   with open(localfile, "rb") as f:
     dbx.files_upload(f.read(), filename, mode=dropbox.files.WriteMode.overwrite)
